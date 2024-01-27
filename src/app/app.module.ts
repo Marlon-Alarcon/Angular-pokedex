9ceb765ef3cd99000a2aa3a046f92c4e9dfad76b
Marlon-Alarcon <89886882+Marlon-Alarcon@users.noreply.github.com>
2024-01-26 20:00:32 -0500

    Mejorando y modificando css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { Detalle_pokemonComponent } from './components/detalle_pokemon/detalle_pokemon.component';
 import { HomeComponent } from './pages/home/home.component';
 import { Foto_pokemonComponent } from './components/foto_pokemon/foto_pokemon.component';
+import { Tarjeta_pokemonComponent } from './components/tarjeta_pokemon/tarjeta_pokemon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     Detalle_pokemonComponent,
-    Foto_pokemonComponent
+    Foto_pokemonComponent,
+    Tarjeta_pokemonComponent
 
   ],
   imports: [
